9df36bc858bcbab557cee9f8f1bcc38f3b5c6a4b
John Riviere <mr.jriviere@gmail.com>
2021-03-08 20:27:36 -0600

    updated pool for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -7,11 +7,13 @@ if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
-  // const auth = params.auth.split(':');
+  const auth = params.auth.split(':');
+  var n = params.pathname.lastIndexOf("/");
+  console.log(n)
 
   config = {
-    // user: auth[0],
-    // password: auth[1],
+    user: auth[0],
+    password: auth[1],
     host: params.hostname,
     port: params.port,
     database: params.pathname.split('/')[1],
